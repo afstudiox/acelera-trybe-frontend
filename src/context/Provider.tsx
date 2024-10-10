73de7b1198e0829759a314508f89ae093c3fb6d3
Andre Santos <afelipes@gmail.com>
2024-10-10 00:13:44 -0300

    tela de tarefas com as funcionalidades concluidas

--- src/context/Provider.tsx
+++ src/context/Provider.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Context from "./Context";
+import { fetchTodos, putTodo, Todo } from "../api/todosApi";
 
 export type ProviderProps = {
     children: React.ReactNode;
@@ -7,22 +8,58 @@ export type ProviderProps = {
 
 export type ProviderValues = {
     user: string;
+    todos: Todo[];
+    loading: boolean;
     onLogin: (username: string) => void;
+    getTodos: () => Promise<void>;
+    editTodos: (taskData: Todo) => Promise<void>;
 }
 
 
 function Provider({ children }: ProviderProps) {
 
     const [user, setUser] = useState('');
+    const [todos, setTodos] = useState<Todo[]>([]);
+    const [loading, setLoading] = useState(false);
 
     const onLogin = (username: string) => {
         setUser(username);
     };
 
+    const getTodos = async () => {
+        try {
+            setLoading(true);
+            const result = await fetchTodos();
+            setTodos(result);
+        } catch (error) {
+            console.log('Erro ao buscar tarefas', error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const editTodos = async (taskData: Todo) => {
+        const updatedTodos = todos.map((task) => {
+            if(task.id === taskData.id) {
+                task.checked = taskData.checked;
+            }
+            return task;
+        });
+        setLoading(true);
+        setTodos(updatedTodos);
+        setLoading(false);
+
+        await putTodo(taskData);
+    };
+
     const values: ProviderValues = {
         user,
-        onLogin
-    };  
+        todos,
+        loading,
+        onLogin, 
+        getTodos,
+        editTodos
+    };
 
     return (
         <Context.Provider value={values}>
--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,13 +1,41 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import Context from "../context/Context";
+import banner from '../assets/todolist_banner.svg';
+import { Todo } from "../api/todosApi";
 
 function TodoList() {
 
-    const { user } = useContext(Context);
+    const { user, todos, loading, getTodos, editTodos } = useContext(Context);
+
+    useEffect(() => {      
+        console.log('UseEffect TodoList');
+        if (!todos.length) {
+            getTodos();
+        }
+    }, [getTodos]);
+
+    function handleChange(task: Todo) {
+        console.log('handleChange', task);
+        editTodos({...task, checked: !task.checked})
+    }
 
     return(
         <>
-        <h1>Welcome, {user} </h1>
+            <h1>Welcome, {user} </h1>
+            <img src={banner} alt="Banner da tela de Todo List" />
+            <button>Add new Task</button>
+            <p>Daily Tasks</p>
+            <ul>
+                {
+                    loading ? <p>Loading...</p> 
+                        : todos.map((task) => {
+                            return <li key={task.id}>
+                                    <input onChange={() => handleChange(task)} type='checkbox' value={task.value} checked={task.checked} />
+                                    {task.value}
+                            </li>
+                        })
+                }
+            </ul>
         </>
     )
 }
